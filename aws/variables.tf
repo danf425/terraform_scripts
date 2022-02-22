e71d7f64f2f0f318ced9002464af58c24faaef33
Dan F <danf425@gmail.com>
2022-02-22 14:03:49 -0500

    scripts

---
+++ aws/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "aws_region is the AWS region in which we will build instances"
+}
+
+variable "aws_profile" {
+  default     = "default"
+  description = "aws_profile is the profile from your credentials file which we will use to authenticate to the AWS API."
+}
+
+variable "aws_credentials_file" {
+  default     = "~/.aws/credentials"
+  description = "aws_credentials_file is the file on your local disk from which we will obtain your AWS API credentials."
+}
