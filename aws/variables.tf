092816a6a8f21af5f4584e2ae0b6bcfa7524712c
Dan Flores Montanez <danf425@gmail.com>
2023-07-17 15:09:56 -0400

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -17,6 +17,10 @@ variable "secret_key" {
   default = "temp"
 }
 
+variable "token" {
+  default = "temp"
+}
+
 #variable "aws_credentials_file" {
 #  default     = "~/.aws/credentials"
 #  description = "aws_credentials_file is the file on your local disk from which we will obtain your AWS API credentials."
